a935efd4c0bbac61f6e4f80290a2456ef4d657a2
ragavellur <raga.vellur@gmail.com>
2026-05-04 23:39:01 +0530

    rebrand: replace all adsb.lol references with bharatradar.com

--- syncmap/overlay.js
+++ syncmap/overlay.js
@@ -333,7 +333,7 @@ function selectStation(stationInfo) {
   $('#si-name').text(stationInfo.name);
   const region = stationInfo.region;
   const euc = encodeURIComponent;
-  const syncUrl = new URL(`/synctable/feeder.html?${euc(region)}&${euc(stationInfo.name)}`, 'https://mlat.adsb.lol/').toString();
+  const syncUrl = new URL(`/synctable/feeder.html?${euc(region)}&${euc(stationInfo.name)}`, 'https://mlat.bharatradar.com/').toString();
   let regionInfo = region ? allRegionInfos.find(ri => ri.region === region) : null;
   if (regionInfo) {
     $('#si-region').text(`${regionInfo.name} (${region})`);
